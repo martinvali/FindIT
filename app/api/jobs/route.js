51f9d6a3f64297c19097e3910fd0c4a2f8481463
unknown <martinvali12@gmail.com>
2023-06-06 17:56:19 +0300

    add job to supabase database from back-end

--- app/api/jobs/route.js
+++ app/api/jobs/route.js
@@ -1,7 +1,17 @@
 import { NextResponse } from "next/server";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
 export async function POST(req) {
   const body = await req.json();
-  console.log(body);
+  const { title, url, location, experience, type } = body;
+
+  const supabase = createServerComponentClient({ cookies });
+  const user = await supabase.auth.getUser();
+
+  const id = user.data.user.id;
+
+  const resp = await supabase.from("posts").insert({ user_id: id, title });
+  console.log(resp);
   return NextResponse.json({}, { status: 200 });
 }
