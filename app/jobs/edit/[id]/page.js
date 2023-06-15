2d50598aefa7bc98224e7978de27bd4840a27ea8
unknown <martinvali12@gmail.com>
2023-06-15 19:32:38 +0300

    Set job editing page's fetch cache to no-store

--- app/jobs/edit/[id]/page.js
+++ app/jobs/edit/[id]/page.js
@@ -1,7 +1,7 @@
 import NewJobPage from "../../new/page";
 
 export default async function Page({ params: { id } }) {
-  const response = await fetch(`http://localhost:3000/api/jobs/${id}`);
+  const response = await fetch(`http://localhost:3000/api/jobs/${id}`, {cache:"no-store"});
   const data = await response.json();
   return <NewJobPage post={...data}></NewJobPage>;
 }
