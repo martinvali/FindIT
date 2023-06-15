4ef0eca08982574de6e5e89204ce949bb1076aa2
unknown <martinvali12@gmail.com>
2023-06-15 09:39:19 +0300

    Dynamically fetch & display blog posts

--- app/blog/hooks/useArticles.js
+++
@@ -1,5 +0,0 @@
-import { useState } from "react";
-
-export function useArticles() {
-  const [articles, setArticles] = useState([]);
-}
--- app/blog/page.js
+++ app/blog/page.js
@@ -1,9 +1,15 @@
-"use client";
-import { useSupabase } from "../providers/SupabaseProvider";
 import { BlogArticle } from "../components/BlogArticle";
 import { HighLightedBlogArticle } from "../components/HighlightedBlogArticle";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-export default function Login() {
+export default async function Login() {
+  const supabase = createServerComponentClient({ cookies });
+
+  const data = await supabase.from("articles").select();
+
+  const articles = data.data;
+
   return (
     <main>
       <section className="pt-24 sm:pt-32 lg:pt-40 outer-container text-center">
@@ -12,13 +18,26 @@ export default function Login() {
         </h1>
 
         <HighLightedBlogArticle
-          title="Lorem ipsum dolor sit amet."
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          date="10 june 2023"
+          title={articles[0].title}
+          description={articles[0].preview}
+          date={articles[0].created_at}
+          id={articles[0].id}
           imgUrl="https://images.unsplash.com/photo-1686726754280-de6be7bd8229?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
         />
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 sm:gap-8 ">
+          {articles.slice(1).map((article) => {
+            return (
+              <BlogArticle
+                key={article.id}
+                title={article.title}
+                id={article.id}
+                description={article.preview}
+                date={article.created_at}
+                imgUrl="https://images.unsplash.com/photo-1686726754280-de6be7bd8229?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+              />
+            );
+          })}
           <BlogArticle
             title="Lorem ipsum dolor sit amet."
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
