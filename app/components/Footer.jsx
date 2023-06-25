26785c5b4ecb58125a912be740f10511b494bb1e
unknown <martinvali12@gmail.com>
2023-06-25 21:44:01 +0300

    Push footer to the bottom of the page

---
+++ app/components/Footer.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+export function Footer() {
+  return (
+    <footer className="outer-container flex flex-col justify-end pt-20 md:pt-24 pb-10 md:pb-14 grow">
+      <Link href="/" className="inline-block mb-4">
+        <img src="/logo.svg" alt="FindIT logo" className="w-20 lg:w-24" />
+      </Link>
+      <section className="md:flex flex-row items-end gap-8 lg:gap-12">
+        <div>
+          <div className="flex flex-row gap-2 mb-2">
+            <img src="/email.svg" alt="Email icon" />
+            <a href="mailto:info@findit.ee" className="text-lg ">
+              info@findit.ee
+            </a>
+          </div>
+          <div className="flex flex-row gap-2 mb-4 md:mb-0">
+            <img src="/phone.svg" alt="Phone icon" />
+            <a href="tel:+37256843090" className="text-lg">
+              +372 5684 3090
+            </a>
+          </div>
+        </div>
+        <p className="text-lg">Copyright © 2023 FindIT. All rights reserved.</p>
+      </section>
+    </footer>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 import { Poppins } from "next/font/google";
 import { Mantine_Provider } from "./providers/MantineProvider.jsx";
 import { SupabaseProvider } from "./providers/SupabaseProvider";
@@ -9,8 +10,6 @@ import { headers, cookies } from "next/headers";
 import { Notifications } from "@mantine/notifications";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import NextTopLoader from "nextjs-toploader";
-import Link from "next/link";
-//export const dynamic = "force-dynamic";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -36,7 +35,7 @@ export default async function RootLayout({ children }) {
   return (
     <html className="bg-white">
       <body
-        className={`${poppins.className} bg-white text-slate-900  user-not-tabbing`}
+        className={`${poppins.className} bg-white overflow-y-scroll	 text-slate-900  user-not-tabbing flex flex-col grow-0`}
       >
         <NextTopLoader color="#06B6D4" showSpinner={false} />
         <SupabaseProvider session={session}>
@@ -45,30 +44,7 @@ export default async function RootLayout({ children }) {
             <Notifications position="top-center" />
             {children}
           </Mantine_Provider>
-          <footer className="outer-container pt-20 md:pt-24 pb-10 md:pb-14">
-            <Link href="/" className="inline-block mb-4">
-              <img src="/logo.svg" alt="FindIT logo" className="w-20 lg:w-24" />
-            </Link>
-            <section className="md:flex flex-row items-end gap-8 lg:gap-12">
-              <div>
-                <div className="flex flex-row gap-2 mb-2">
-                  <img src="/email.svg" alt="Email icon" />
-                  <a href="mailto:info@findit.ee" className="text-lg ">
-                    info@findit.ee
-                  </a>
-                </div>
-                <div className="flex flex-row gap-2 mb-4 md:mb-0">
-                  <img src="/phone.svg" alt="Phone icon" />
-                  <a href="tel:+37256843090" className="text-lg">
-                    +372 5684 3090
-                  </a>
-                </div>
-              </div>
-              <p className="text-lg">
-                Copyright © 2023 FindIT. All rights reserved.
-              </p>
-            </section>
-          </footer>
+          <Footer />
         </SupabaseProvider>
       </body>
     </html>
