ce7219d0a9cd779eb5b227bfe4e81575b4bf94cc
unknown <martinvali12@gmail.com>
2023-06-26 09:49:43 +0300

    Protect job edit & post routes from unauthorized users in middleware

--- middleware.js
+++ middleware.js
@@ -9,7 +9,12 @@ export async function middleware(req) {
     data: { session },
   } = await supabase.auth.getSession();
 
-  if (!session && req.nextUrl.pathname === "/dashboard") {
+  if (
+    (!session && req.nextUrl.pathname === "/dashboard") ||
+    (!session && req.nextUrl.pathname.includes("/jobs/edit/")) ||
+    (!session && req.nextUrl.pathname.includes("/jobs/new"))
+  ) {
+    console.log("no session");
     const url = new URL(req.url);
     url.pathname = "/login";
     return NextResponse.redirect(url);
