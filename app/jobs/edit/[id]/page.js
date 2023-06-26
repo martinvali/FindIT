d7a07b358f64171372fe2192fe4f433b214adaf9
unknown <martinvali12@gmail.com>
2023-06-26 11:59:00 +0300

    add APPLICATION_URL env variable

--- app/jobs/edit/[id]/page.js
+++ app/jobs/edit/[id]/page.js
@@ -3,7 +3,7 @@ import NewJobPage from "../../new/page";
 export const revalidate = 0;
 
 export default async function Page({ params: { id } }) {
-  const response = await fetch(`http://localhost:3000/api/jobs/${id}`, {cache:"no-store"});
+  const response = await fetch(`${process.env.APPLICATION_URL}/api/jobs/${id}`, {cache:"no-store"});
   const data = await response.json();
   return <NewJobPage post={...data}></NewJobPage>;
 }
